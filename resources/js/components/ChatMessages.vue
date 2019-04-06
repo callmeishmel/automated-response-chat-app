5c6bcb3e96e6fb67856c195dea9b6fb118a52b9f
Ismael Guajardo <ismael@ileadserve.com>
2019-04-05 19:29:31 -0600

    Initial code commit

---
+++ resources/js/components/ChatMessages.vue
@@ -0,0 +1,49 @@
+<template>
+    <ul class="chat">
+        <li class="left clearfix p-3" v-for="message in messages">
+            <div class="chat-body clearfix">
+                <div class="header">
+                    <strong class="primary-font">
+                        {{ message.user.name }}
+                    </strong>
+                </div>
+                <p>
+                    {{ message.message }}
+                </p>
+            </div>
+        </li>
+    </ul>
+</template>
+
+<script>
+
+  export default {
+    props: ['messages', 'user'],
+
+    data() {
+      return {
+
+      }
+    },
+
+    watch: {
+      'messages': {
+        handler: function(messages) {
+          setTimeout(function() {
+              $('.chat-messages').scrollTop($('.chat-messages')[0].scrollHeight);
+          }, 0);
+        }
+      }
+    },
+
+    methods: {
+      scrollToBottomOfMessages() {
+        $('.chat-messages').scrollTop($('.chat-messages')[0].scrollHeight);
+      }
+    },
+
+    created() {
+    }
+
+  };
+</script>
---
+++ resources/views/chat.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div id="chat-panel-wrapper" class="row">
+  <div class="col-md-2 chat-sidebar">
+    Chat Sidebar
+  </div>
+
+  <div class="col-md-10 p-0 chat-content">
+
+    <div class="chat-messages h-75">
+      <chat-messages
+        :messages="messages"
+        :user="{{ Auth::user() }}"
+      ></chat-messages>
+    </div>
+
+    <div class="chat-message-form h-25 p-3">
+      <chat-form
+          v-on:messagesent="addMessage"
+          :user="{{ Auth::user() }}"
+      ></chat-form>
+    </div>
+
+  </div>
+</div>
+
+@endsection
