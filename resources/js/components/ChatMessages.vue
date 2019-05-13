callmeishmel/automated-response-chat-app

commit e33ff27f35f7558d179365874ca75f7b30265701

Cleared currentContact notifications by creating a current_contact column tracker on the users table

resources/js/components/ChatMessages.vue
@@ -109,6 +109,22 @@
               $('.chat-messages').scrollTop($('.chat-messages')[0].scrollHeight);
           }, 0);
         }
+      },
+
+      // Not proud of this one but this is the one quick way to clear the current
+      // contact's notifications that cannot be caught by clicking on the contact
+      // name on the sidebar
+      'userContacts': {
+        handler: function(contacts) {
+          if(this.user.current_contact !== null) {
+
+            for(let index in this.userContacts) {
+              if(this.userContacts[index].id === this.user.current_contact) {
+                this.setNewContact({id:this.user.current_contact, name:this.userContacts[index].name});
+              }
+            }
+          }
+        }
       }
     },
 

app/Http/Controllers/ChatsController.php
@@ -114,6 +114,19 @@ class ChatsController extends Controller
     return ['user_id' => Auth::user()->id, 'api_token' => Auth::user()->api_token];
   }
 
+  public function setUserCurrentContact($contactId)
+  {
+    $user = Auth::user();
+
+    $user->current_contact = $contactId;
+
+    if($user->save()) {
+      return ['success' => true, 'msg' => 'Current contact succesfully updated.'];
+    } else {
+      return ['success' => false, 'msg' => 'Current contact update failed.'];
+    }
+  }
+
   /**
   * Get responses for a canned message
   *

database/migrations/2019_05_10_144630_add_current_contact_column_to_users_table.php
@@ -1,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddCurrentContactColumnToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->integer('current_contact')->after('status')->nullable()->default(null);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+      //
+    }
+}

public/js/app.js
@@ -1819,21 +1819,25 @@ function _defineProperty(obj, key, value) { if (key in obj) { Object.definePrope
   computed: _objectSpread({}, Object(vuex__WEBPACK_IMPORTED_MODULE_0__["mapState"])('chatStore', ['currentContact', 'currentContactName', 'contactNotifications', 'userContacts'])),
   methods: _objectSpread({}, Object(vuex__WEBPACK_IMPORTED_MODULE_0__["mapMutations"])('chatStore', ['setNewContactInStore', 'removeFromContactNotifications', 'getUserContacts']), {
     setNewContact: function setNewContact(contact) {
+      var _this = this;
+
       this.setNewContactInStore(contact);
 
       if (this.contactNotifications.includes(contact.id)) {
         this.removeFromContactNotifications(contact.id);
       }
 
-      axios.get('remove-contact-notification/' + contact.id).then(function () {});
+      axios.get('remove-contact-notification/' + contact.id).then(function () {
+        axios.get('set-user-current-contact/' + _this.currentContact);
+      });
     }
   }),
   mounted: function mounted() {
-    var _this = this;
+    var _this2 = this;
 
     this.getUserContacts(this.user.api_token);
     setInterval(function () {
-      _this.getUserContacts(_this.user.api_token);
+      _this2.getUserContacts(_this2.user.api_token);
     }, 30000);
 
     for (var i = 0; i < this.contactNotificationsProp.length; i++) {
@@ -2086,6 +2090,23 @@ function _defineProperty(obj, key, value) { if (key in obj) { Object.definePrope
         setTimeout(function () {
           $('.chat-messages').scrollTop($('.chat-messages')[0].scrollHeight);
         }, 0);
+      }
+    },
+    // Not proud of this one but this is the one quick way to clear the current
+    // contact's notifications that cannot be caught by clicking on the contact
+    // name on the sidebar
+    'userContacts': {
+      handler: function handler(contacts) {
+        if (this.user.current_contact !== null) {
+          for (var index in this.userContacts) {
+            if (this.userContacts[index].id === this.user.current_contact) {
+              this.setNewContact({
+                id: this.user.current_contact,
+                name: this.userContacts[index].name
+              });
+            }
+          }
+        }
       }
     }
   },
@@ -67706,7 +67727,7 @@ if (token) {
 window.Pusher = __webpack_require__(/*! pusher-js */ "./node_modules/pusher-js/dist/web/pusher.js");
 window.Echo = new laravel_echo__WEBPACK_IMPORTED_MODULE_0__["default"]({
   broadcaster: 'pusher',
-  key: "1dba86919d5a0c3e0bbf",
+  key: "0635d848acd6511fc6af",
   cluster: "us3",
   encrypted: true
 });

resources/js/components/ChatContacts.vue
@@ -77,7 +77,9 @@ export default {
         this.removeFromContactNotifications(contact.id);
       }
 
-      axios.get('remove-contact-notification/' + contact.id).then(() => {});
+      axios.get('remove-contact-notification/' + contact.id).then(() => {
+        axios.get('set-user-current-contact/' + this.currentContact);
+      });
     }
 
   },

routes/web.php
@@ -20,6 +20,7 @@ Route::get('messages/{contactId?}', 'ChatsController@fetchMessages');
 Route::post('messages', 'ChatsController@sendMessage');
 
 Route::get('current-app-user', 'ChatsController@getCurrentAppUser');
+Route::get('set-user-current-contact/{contactId}', 'ChatsController@setUserCurrentContact');
 Route::get('user-contacts', 'ChatsController@getUserContacts');
 Route::get('add-contact-notification/{contactId}', 'ChatsController@addContactNotification');
 Route::get('remove-contact-notification/{contactId}', 'ChatsController@removeContactNotification');

commit aedc4f494372e7590aed84b8252be010d852f1fd

Fixed bug introduced in commit e33ff27 where app kept overriding current contact. It needed to check if store current contact was null.

resources/js/components/ChatMessages.vue
@@ -116,7 +116,7 @@
       // name on the sidebar
       'userContacts': {
         handler: function(contacts) {
-          if(this.user.current_contact !== null) {
+          if(this.user.current_contact !== null && this.currentContact === null) {
 
             for(let index in this.userContacts) {
               if(this.userContacts[index].id === this.user.current_contact) {